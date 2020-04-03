7aff3372f4708fda536ca22da2d6de4c5ddbb631
IhorChupryna <chuprina.ihor@gmail.com>
2020-04-03 14:51:26 +0300

    hw3

--- lesson3/main_lesson.js
+++ lesson3/main_lesson.js
@@ -7,27 +7,37 @@
 Шаг можно не указывать, тогда он будет равен одному. 
 Начальное значение по умолчанию равно 0. Генераторов можно создать сколько угодно.
 
-let generator = function(start, step) {
-    return function() {
-
-    }
+2) Также, нужна функция take(gen, x) которая вызвает функцию gen заданное число (x) раз и возвращает массив с результатами вызовов.                                              
+*/
+//--------------------------------> 1 <--------------------------------//
+function sequence(start, step) {
+  if (start === undefined)
+    start = 0;
+  else
+    start -= step;
+  return function () {
+    return start += step;
+  }
 }
 
-ПРИМЕР:
-var generator = sequence(10, 3);
-var generator2 = sequence(7, 1);
+let generator = sequence(10, 3);
+let generator2 = sequence(7, 1);
 
-console.log(generator()); // 10
-console.log(generator()); // 13
+console.log(generator());   // 10
+console.log(generator());   // 13
+console.log(generator2());  // 7
+console.log(generator());   // 16
+console.log(generator2());  // 8
+//--------------------------------> 1 <--------------------------------//
 
-console.log(generator2()); // 7
-
-console.log(generator()); // 16
-
-console.log(generator2()); // 8
-
-2) Также, нужна функция take(gen, x) которая вызвает функцию gen заданное число (x) раз и возвращает массив с результатами вызовов.
-    var gen2 = sequence(0, 2);
-    console.log(take(gen2, 5)); [0, 2, 4, 6, 8 ]                                                     +
-
-*/
+//--------------------------------> 2 <--------------------------------// 
+let gen2 = sequence(0, 2);
+function take(func,len){
+  let array=new Array(len);
+  for (let i = 0; i < array.length; i++) {
+      array[i]=func();
+  }
+  return array;
+}
+console.log(take(gen2, 5)); [0, 2, 4, 6, 8 ]                                                     
+//--------------------------------> 2 <--------------------------------//
